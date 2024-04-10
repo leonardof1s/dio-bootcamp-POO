fba5b39d7072a11aa3bd891356221fd7bd363439
leonardof1s <ferreiradesouza063@gmail.com>
2024-04-09 22:05:39 -0300

    classes criadas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.desafio.dominio.Curso;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao do curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Curso Python");
+        curso1.setDescricao("Descricao do curso Python");
+        curso1.setCargaHoraria(8);
+
+
+
+        List<Curso> minhalista  = new LinkedList<>(){{
+            add(curso1);
+            add(curso2);
+        }};
+
+
+        System.out.println(minhalista.toString());
     }
 }
